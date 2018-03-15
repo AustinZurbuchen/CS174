97ff3b6b90d9978a2c381e896543b2c9e71a1a33
Austin Zurbuchen <azzbuch129@gmail.com>
2018-03-15 03:56:58 -0700

    Updating hw files

--- hw4/hw4.php
+++ hw4/hw4.php
@@ -3,10 +3,38 @@
 <body>
 
 <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+	$num = $_POST["number"];
+	if ($num < 1){
+		echo "<h2>ERROR!</h2><br>";
+		echo "The number you entered is less than 1 <br>";
+		echo "Enter a number greater than or equal to 1 <b>";
+	} else {
+		echo "For n = " . $num . ", index is " . indexOfFibNDigits($num) . "!";
+	}
+
+	function indexOfFibNDigits($n){
+		if ($n == 1){
+			return $n;
+		} else {
+			$prev = 1;
+			$secPrev = 1;
+			$index = 3;
+			$fibVal = 0;
+			//$break = false;
+			while(true){
+				$fibVal = $prev + $secPrev;
+				//echo "index is $index | fibVal is $fibVal <br>";
+				$fibValLen = strlen((string)$fibVal);
+				if($fibValLen == $n){
+					return $index;
+				} else {
+					$secPrev = $prev;
+					$prev = $fibVal;
+					$index++;
+				}
+			}
+		}
+	}
 ?> 
 
 </body>
